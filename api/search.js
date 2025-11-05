eb668583877493f8add5ccbc5c4e717fce439733
darkmagiciangirl2025 <sam.stone838@gmail.com>
2025-11-04 21:55:37 -0600

    Update search.js

--- api/search.js
+++ api/search.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       `&REST-PAYLOAD` +
       `&keywords=${encodeURIComponent(query)}` +
       `&paginationInput.entriesPerPage=20` +
-      `&sortOrder=StartTimeNewest` +
+      `&sortOrder=PricePlusShippingHighest` +
       `&itemFilter(0).name=MinPrice` +
       `&itemFilter(0).value=1000` +
       `&itemFilter(0).paramName=Currency` +
